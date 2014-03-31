e0db750d7b352e93e7a692cfaa81d220215e58ca
Maxwell Friederichs <friederichsm@uwplatt.edu>
2014-03-30 23:38:39 -0500

    working

--- server/index.js
+++ server/index.js
@@ -1,27 +1,6 @@
-/**
- * Module dependencies.
- */
+var express = require('express');
+var app = express();
 
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    fs = require('fs');
+app.use(express.static(__dirname + '/../public'));
 
-/**
- * Main application entry file.
- * Please note that the order of loading is important.
- */
- 
-app.use(express.static(__dirname + '/public', { maxAge: 0 }));
-app.use(express.bodyParser());
-
-require('./config/routes')(app);
-
-// express settings
-
-var port = 3000;
-server.listen(port);
-console.log('Express app started on port ' + port);
-
-// expose app
-exports = module.exports = app;
+app.listen(process.env.PORT || 3000);
