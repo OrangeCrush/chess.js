1c3425a0749029c2efddf9ccdacc5bfbb929e2f9
Max Friederichs <friederichsm@uwplatt.edu>
2014-04-11 12:53:30 -0500

    shifted my design towards game class.  I think this is the best approach

--- public/js/src/board.js
+++ public/js/src/board.js
@@ -7,34 +7,28 @@ function Board(init){
 }
 
 Board.prototype.clearBoard = function(){
-   this.board = new Array(8);
+   this.squares = new Array(8);
    for(var i = 0 ; i < 8; i++){
-      this.board[i] = new Array(8);
+      this.squares[i] = new Array(8);
+      for(var j = 0; j < 8; j++){
+         this.squares[i][j] = {
+            occupied: false,
+            piece: null
+         };              
+      }
    }
-   this.white = {};
-   this.white = {};
 }
 
 Board.prototype.newGame = function(){
    this.clearBoard();
-
-   for(var i = 0; i < 8; i++){
-      //White
-      this.board[0][i] = buildPieceByCol(i, {xpos: i, ypos: 0});
-      this.board[1][i] = new Piece({xpos:i, ypos:1});
-
-      //Black
-      this.board[6][i] = new Piece({xpos:i, ypos:6});
-      this.board[7][i] = buildPieceByCol(i, {xpos: i, ypos: 7});
-   }
 }
 
 Board.prototype.toString = function(){
    var rval = '\n';
-   for(var i=0; i < 8; i ++){
+   for(var i = 0; i < 8; i ++){
       for(var j = 0; j < 8; j++){
-         if(this.board[i][j]){
-            rval += this.board[i][j].toString() + ' ';
+         if(this.squares[i][j].piece){
+            rval += this.squares[i][j].piece.toString() + ' ';
          }else{
             rval += ' ';
          }
@@ -44,23 +38,6 @@ Board.prototype.toString = function(){
    return rval;
 }
 
-function buildPieceByCol(col, init){
-   switch(col){
-      case 0:
-      case 7:
-         return new Rook(init);
-      case 1:
-      case 6:
-         return new Knight(init);
-      case 2:
-      case 5:
-         return new Bishop(init);
-      case 3:
-         return new Queen(init);
-      case 4:
-         return new King(init);
-   }
-}
 
 /*
  * The big daddy.  Process a move.
@@ -72,63 +49,12 @@ Board.prototype.processMove = function(pgn){
    if(!isPGNCastle(pgn)){
       var spot_from = pgn.match(/([a-z0-9]+)[x-]/i)[1]
       if(isPieceAtPGNsqr.call(this, spot_from)){
-         
+
       }
       else{
          if(canCastle(pgn)){
 
          }
       }
+   }
 }
-
-function isPieceAtPGNsqr(spot){
-   var piece_type = spot[0];
-   var board_column = letters[spot[1]];
-   var board_row = +spot[2] - 1;
-   var piece_from = this.board[board_row][board_column];
-
-   // Check if the piece is what it says it is
-   return piece_from.name === piece_type;
-}
-
-function isPGNCastle(pgn){
-   return pgn === 'O-O-O' || pgn === 'O-O';
-}
-
-function canPieceMoveTo(piece, dest){
-   
-}
-
-function isPieceBlockedFromSqr(){
-
-}
-
-function isCheck(){
-
-}
-
-function isCheckMate(){
-
-}
-
-/*
-*
-*/
-function canCastle(pgn){
-
-}
-
-var letters = {
-   a:0,
-   b:1,
-   c:2,
-   d:3,
-   e:4,
-   f:5,
-   g:6,
-   h:7
-}
-
-var b = new Board();
-b.newGame();
-console.log(b.toString());
--- public/js/src/game.js
+++ public/js/src/game.js
@@ -1,3 +1,98 @@
 function Game(){
+   this.board = new Board();
+   this.black = this.white = [];
+   this.blackKing = this.whiteKing = null;
+}
 
+Game.prototype.newGame = function(){
+   this.black = this.white = [];
+   for(var i = 0; i < 8; i++){
+
+      var b1 = buildPieceByCol(i, {xpos: i, ypos: 7});
+      var b0 = new Piece({xpos:i, ypos:6});
+
+      var w1 = new Piece({xpos:i, ypos:1});
+      var w0 = buildPieceByCol(i, {xpos: i, ypos: 0});
+
+      this.white.push(w0);
+      this.white.push(w1);
+      this.black.push(b0);
+      this.black.push(b1);
+
+      this.board.squares[0][i] = {occupied: true, piece: w0}; 
+      this.board.squares[1][i] = {occupied: true, piece: w1};
+      this.board.squares[6][i] = {occupied: true, piece: b0};
+      this.board.squares[7][i] = {occupied: true, piece: b1};
+
+      if(w0.toString() === 'K')
+         this.whiteKing = w0;
+      if(b1.toString() === 'K')
+         this.blackKing = b1;
+   }
+}
+
+Game.prototype.validateMove = function(pngMove){
+
+}
+
+function buildPieceByCol(col, init){
+   switch(col){
+      case 0:
+      case 7:
+         return new Rook(init);
+      case 1:
+      case 6:
+         return new Knight(init);
+      case 2:
+      case 5:
+         return new Bishop(init);
+      case 3:
+         return new Queen(init);
+      case 4:
+         return new King(init);
+   }
+}
+
+function isPieceAtPGNsqr(spot){
+   var piece_type = spot[0];
+   var board_column = letters[spot[1]];
+   var board_row = +spot[2] - 1;
+   var piece_from = this.board.squares[board_row][board_column];
+
+   // Check if the piece is what it says it is
+   return piece_from.name === piece_type;
+}
+
+function isPGNCastle(pgn){
+   return pgn === 'O-O-O' || pgn === 'O-O';
+}
+
+function canPieceMoveTo(piece, dest){
+   
+}
+
+function isCheck(){
+
+}
+
+function isCheckMate(){
+
+}
+
+/*
+*
+*/
+function canCastle(pgn){
+
+}
+
+var letters = {
+   a:0,
+   b:1,
+   c:2,
+   d:3,
+   e:4,
+   f:5,
+   g:6,
+   h:7
 }
