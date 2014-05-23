68a4f57104a643790e8ea5e7c47d0c9e10c592cb
Maxwell Friederichs <friederichsm@uwplatt.edu>
2014-05-23 02:10:12 -0500

    The game room manager handles emitting to clients as well as handling game management

---
+++ server/gameRoomManager.js
@@ -0,0 +1,106 @@
+if(typeof define !== 'function'){
+   var define = require('amdefine')(module);
+}
+define(function(require, exports, module){
+   var Game = require('Game');
+   //map from game ids to game objects currently being player
+
+   /*
+    * This class is primarily responsible for the management of
+    * player sockets as well as game validation and setting up
+    */
+   function GameRoomManager(init){
+      this.games = [];
+      this.waiting = [];
+      this.iosockets = init.iosockets;
+   }
+
+   /*
+    * queue up waiting player's sockets.
+    */
+   GameRoomManager.prototype.joinGame = function(playerSocket){
+      this.waiting.push(playerSocket);
+   }
+
+   /*
+    * Called in a 5 second interval above.
+    * Creates a serverside instance of the game for validation
+    * and emits a begin game event to each players socket
+    */
+   GameRoomManager.prototype.generateNewGame = function(player1Socket, player2Socket, gameOpts){
+      var id = this.generateId();
+      this.games.push({
+         id: id,
+         game: new Game(),
+         player1: player1Socket,
+         player2: player2Socket
+      });
+
+      //Spin off a socketio room
+      player1Socket.join(id);
+      player2Socket.join(id);
+
+      //Emit startGame event.  Client will draw board
+      //player 1 is always white, player 2 is always black
+      player1Socket.emit('startGame', {
+         gameId: id,
+         color: 'white'
+      });
+
+      player2Socket.emit('startGame', {
+         gameId: id,
+         color: 'black'
+      });
+
+      return id;
+   }
+
+   GameRoomManager.prototype.runMove = function(id, color, pgnMove){
+      var room = this.getGameById(id);
+      var game = room.game;
+      var erMsg;
+      if((erMsg = game.validateMove(pgnMove, color)).valid){
+         game.processMove(pgnMove, color);
+         console.log('clean move broadcasting')
+         this.iosockets.in(id).emit('newMove', {
+            color: color,
+            pgnMove: pgnMove
+         });
+      }else if(color === 'white'){
+         room.player1.emit('badMove', {
+            pgnMove: pgnMove,
+            msg: erMsg.desc,
+            rollBack: game
+         });
+      }else if(color === 'black'){
+         room.player2.emit('badMove',{
+            pgnMove: pgnMove,
+            msg: erMsg.desc,
+            rollBack: game
+         });
+      }
+   }
+
+
+   GameRoomManager.prototype.generateId = function(){
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for( var i = 0; i < 50; i++ ){
+         text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      if(!this.games[text]){
+         return text;
+      }else{
+         return this.generateId(); //recurse and get a new random id..I never expect this to get hit
+      }
+   }
+
+   GameRoomManager.prototype.getGameById = function(id){
+      return this.games.filter(function(x){
+         return x.id === id;
+      })[0];
+   }
+
+   return GameRoomManager;
+});
